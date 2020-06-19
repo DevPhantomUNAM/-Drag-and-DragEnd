bbf98c38e206a7781557ab1e85ae0e7cacff922d
Dev Phantom <Dev.Phantom.UNAM@gmail.com>
2020-06-19 18:15:06 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,63 @@
+class DOMHelper{
+    static move(el,coords){
+        el.style.top = (coords.y - (el.clientHeight/2  )) +"px";
+        el.style.left = (coords.x - (el.clientWidth /2  )) +"px";
+    }
+}
+
+class DragList{
+    constructor(list_selector, items_selector='li'){
+        this.list = document.querySelector(list_selector);
+        this.items = this.list.querySelectorAll(items_selector);
+
+        this.handleDragStart = this.handleDragStart.bind(this);
+        this.handleDrag = this.handleDrag.bind(this);
+        this.handleDragEnd = this.handleDragEnd.bind(this);
+
+        this.canvas = document.createElement("canvas")
+
+
+        this.bindEvents();
+    }
+
+    bindEvents(){
+        this.items.forEach(item=>{
+            item.addEventListener("dragstart",this.handleDragStart);
+            item.addEventListener("drag",this.handleDrag);
+            item.addEventListener("dragend",this.handleDragEnd);
+        })
+    }
+
+
+    handleDragStart(ev){
+        let el = ev.currentTarget;
+        el.classList.add("dragging")
+        
+        ev.dataTransfer.setDragImage(this.canvas,0,0);
+    }
+    handleDrag(ev){
+        DOMHelper.move(ev.currentTarget,{
+            x: ev.clientX,
+            y: ev.clientY
+        })
+
+    }
+    handleDragEnd(ev){
+        let el = ev.currentTarget;
+        el.classList.remove("dragging")
+        
+        el.style.top = "";
+        el.style.left = "";
+    }
+
+}
+
+// (function () {
+//     console.log("holi");
+    
+//     new DragList("ul");
+// })
+
+
+console.log("holi");
+new DragList("ul");
